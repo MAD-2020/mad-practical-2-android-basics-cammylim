e19c7922846a4940d278ff17e584e87d59593f3c
cammylim <64000790+cammylim@users.noreply.github.com>
2020-05-03 16:13:48 +0800

    Edited the score system and added log outputs

--- MainActivity.java
+++ MainActivity.java
@@ -16,32 +16,30 @@ public class MainActivity extends AppCompatActivity {
     private Button ButtonRight;
     private TextView Score;
     private static int count = 0;
-    private static int molecount = 3;
-    private static final String TAG = "ButtonActivity";
-    private static int i = 0;
+    private static final String TAG = "Whack-A-Mole";
 
-    public void Randomizer(){
+    public void Randomizer() {
         Button[] buttons = {ButtonLeft, ButtonMiddle, ButtonRight};
         Random random = new Random();
         int select = random.nextInt(3);
         Button m = buttons[select];
-        for (Button b : buttons){
-            if (b == m){
+        for (Button b : buttons) {
+            if (b == m) {
                 b.setText("*");
-            }
-            else{
+            } else {
                 b.setText("O");
             }
         }
     }
-    public void UpdateScore(){
-        Score.setText(Integer.toString(count++));
+
+    public void UpdateScore() {
+        Score.setText(String.valueOf(count));
     }
-    public boolean Check(Button b){
-        if ( b.getText() == "*"){
+
+    public boolean Check(Button b) {
+        if (b.getText() == "*") {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
@@ -54,23 +52,42 @@ public class MainActivity extends AppCompatActivity {
         ButtonMiddle = (Button) findViewById(R.id.ButtonMiddle);
         ButtonRight = (Button) findViewById(R.id.ButtonRight);
         Score = (TextView) findViewById(R.id.Score);
-        Randomizer();
     }
+
     @Override
-    public void onStart(){
+    public void onStart() {
         super.onStart();
         Log.d(TAG, "Application launched!");
         Randomizer();
         UpdateScore();
     }
-    public void OnClickButton(View v){
-        Button button = (Button) v;
-        if (Check(button) == true){
-            UpdateScore();
+
+    public void OnClickButton(View v) {
+        switch(v.getId()){
+            case R.id.ButtonLeft:
+                Log.v(TAG,"Left Button Clicked!");
+                break;
+
+            case R.id.ButtonMiddle:
+                Log.v(TAG,"Middle Button Clicked!");
+                break;
+
+            default:
+                Log.v(TAG,"Right Button Clicked!");
         }
+        Button button = (Button) v;
+        if (Check(button) == true) {
+            count++;
+            Log.v(TAG, "Hit, score added!");
+        }
+        else if(count != 0){
+            count--;
+            Log.v(TAG, "Missed, score deducted!");
+        }
+        else{
+            Log.v(TAG, "Missed, score deducted!");
+        }
+        UpdateScore();
         Randomizer();
     }
-
-
 }
-
